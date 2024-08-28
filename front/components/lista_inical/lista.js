ffa56d0ca6a88741c0ec0b19992afc517be21834
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-08-28 18:28:30 -0300

    Criacao dos botoes retrateis da lista

--- front/components/lista_inical/lista.js
+++ front/components/lista_inical/lista.js
@@ -1,33 +1,52 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../../styles/lista_inicial/lista.module.css";
 
 export default function Lista({ jobs = [] }) {
-  const [open, setOpen] = useState(false);
-  const [openImage, setOpenImage] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(!open);
-  };
-  const handleOpenImage = () => {
-    setOpenImage(!openImage);
+  //recebe o job id e a variavel que ira controlar o estado de aberto e fechado de cada um
+  const [openDescription, setOpenDescription] = useState([]);
+  const [openImage, setOpenImage] = useState([]);
+
+
+
+  // inicia o estado fechado
+  useEffect(()=>{
+      
+      const initialState = jobs.reduce((acc, job) => {
+        acc[job.id] = false;
+        return acc;
+      }, {});
+
+      setOpenImage(initialState)
+      setOpenDescription(initialState)
+
+
+  },[jobs])
+  
+  // cuida da alteracao de estado
+  const handleButtonClick = (id, setObject) => {
+    setObject(prevState => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
   };
 
-  //primeira div deve ser um container relativo
+
   return (
     <>
       {jobs.map((job) => (
-        <div className={styles.container}>
+        <div className={styles.container} key={job.id}>
           <div>
-            {console.log(job)}
             <h1>{job.job_name}</h1>
-            <button onClick={() => handleOpen()}>Abre e fecha</button>
-            <button onClick={() => handleOpenImage()}>Incone imagem</button>
+            <button onClick={() => handleButtonClick(job.id, setOpenDescription)}>Descricao</button>
+            <button onClick={() => handleButtonClick(job.id, setOpenImage)}>Incone imagem</button>
+            <button >Delete</button>
           </div>
           <div>
-            {open && <p>{job.job_description}</p>}</div>
-            {openImage && (
+            {openDescription[job.id] && <p>{job.job_description}</p>}</div>
+            {openImage[job.id] && (
             <div className={styles.image}>
               <p>{job.image}</p>
             </div>
