bb0c9b57dc6cc5262150e83f0b1bada45a731740
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-08-28 17:40:01 -0300

    modelo inicial da lista

--- front/components/lista_inical/lista.js
+++ front/components/lista_inical/lista.js
@@ -1,9 +1,38 @@
+"use client";
 
-export default function Inicio() {
-    return (
-      <>
-      <h1>Lista teste</h1>
-      </>
-  
-    );
-  }
+import { useState } from "react";
+import styles from "../../styles/lista_inicial/lista.module.css";
+
+export default function Lista({ jobs = [] }) {
+  const [open, setOpen] = useState(false);
+  const [openImage, setOpenImage] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  const handleOpenImage = () => {
+    setOpenImage(!openImage);
+  };
+  console.log(jobs)
+  //primeira div deve ser um container relativo
+  return (
+    <>
+      {jobs.map((job) => (
+        <div className={styles.container}>
+          <div>
+            <h1>{job.name}</h1>
+            <button onClick={() => handleOpen()}>Abre e fecha</button>
+            <button onClick={() => handleOpenImage()}>Incone imagem</button>
+          </div>
+          <div>
+            {open && <p>{job.description}</p>}</div>
+            {openImage && (
+            <div className={styles.image}>
+              <p>{job.image}</p>
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  );
+}
