8bb8fe09097d6fa87e5157c16ba3956851f1af8e
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-08-28 17:40:22 -0300

    modelo incial da lista

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,9 +1,15 @@
 "use client"; 
 
 import Image from "next/image";
+import Lista from '../../components/lista_inical/lista.js'
 import styles from "../../styles/lista_inicial/lista_inicial.module.css"
 import { useEffect, useState } from "react";
 
+const jobs = [
+  { name: 'rafael', description: 'descricao', image: 'imagem' },
+  { name: 'maria', description: 'outra descricao', image: 'outra_imagem' },
+  { name: 'joao', description: 'mais uma descricao', image: 'mais_uma_imagem' }
+];
 
 //falta implementar
 const handleClick = () => {
@@ -20,6 +26,9 @@ export default function Job() {
 
       <section>
         <button >Adicionar Job</button>
+      </section>
+      <section>
+        <Lista jobs={jobs}/>
       </section>
 
     </main>
