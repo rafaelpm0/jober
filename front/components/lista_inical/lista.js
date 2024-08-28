a61b7a7f4e2cd2318f1eca921d7f9ecc76a55de8
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-08-28 19:33:11 -0300

    funcionalidade imagem indisp. botao sem efeito

--- front/components/lista_inical/lista.js
+++ front/components/lista_inical/lista.js
@@ -5,35 +5,36 @@ import styles from "../../styles/lista_inicial/lista.module.css";
 import handleDelete from "../../api/del_job";
 
 export default function Lista({ jobs = [], setJobs }) {
-
   //recebe o job id e a variavel que ira controlar o estado de aberto e fechado de cada um
   const [openDescription, setOpenDescription] = useState([]);
+  const [dispImage, setDispImage] = useState([]);
   const [openImage, setOpenImage] = useState([]);
 
-
-
   // inicia o estado fechado
-  useEffect(()=>{
-      
-      const initialState = jobs.reduce((acc, job) => {
-        acc[job.id] = false;
-        return acc;
-      }, {});
+  useEffect(() => {
+    const initialState = jobs.reduce((acc, job) => {
+      acc[job.id] = false;
+      return acc;
+    }, {});
 
-      setOpenImage(initialState)
-      setOpenDescription(initialState)
+    setOpenImage(initialState);
+    setOpenDescription(initialState);
 
+    jobs.map((job) => {
+      setDispImage((prevState) => ({
+        ...prevState,
+        [job.id]: job.has_image,
+      }));
+    });
+  }, [jobs]);
 
-  },[jobs])
-  
   // cuida da alteracao de estado
-  const handleHide= (id, setObject) => {
-    setObject(prevState => ({
+  const handleHide = (id, setObject) => {
+    setObject((prevState) => ({
       ...prevState,
-      [id]: !prevState[id]
+      [id]: !prevState[id],
     }));
   };
-
 
   return (
     <>
@@ -41,13 +42,27 @@ export default function Lista({ jobs = [], setJobs }) {
         <div className={styles.container} key={job.id}>
           <div>
             <h1>{job.job_name}</h1>
-            <button onClick={() => handleHide(job.id, setOpenDescription)}>Descricao</button>
-            <button onClick={() => handleHide(job.id, setOpenImage)}>Incone imagem</button>
-            <button onClick={() => handleDelete(setJobs, jobs, job.id)}>Delete</button>
+            <button onClick={() => handleHide(job.id, setOpenDescription)}>
+              Descricao
+            </button>
+
+            {dispImage[job.id] ? (
+              <button onClick={() => handleHide(job.id, setOpenImage)}>
+                Incone imagem
+              </button>
+            ) : (
+              <button>Imagem indisponivel</button>
+            )}
+            <button onClick={() => handleDelete(setJobs, jobs, job.id)}>
+              Delete
+            </button>
           </div>
-          <div>
-            {openDescription[job.id] && <p>{job.job_description}</p>}</div>
-            {openImage[job.id] && (
+          {openDescription[job.id] && (
+            <div>
+              <p>{job.job_description}</p>{" "}
+            </div>
+          )}
+          {openImage[job.id] && (
             <div className={styles.image}>
               <p>{job.image}</p>
             </div>
