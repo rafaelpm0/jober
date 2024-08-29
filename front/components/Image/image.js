c56a38eb6cc697d02806a6d37bec1a662a481e13
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-08-29 18:37:17 -0300

    estilizacao do componente imagem

--- front/components/Image/image.js
+++ front/components/Image/image.js
@@ -1,5 +1,6 @@
 import getImage from "../../api/get_image";
 import { useEffect, useState } from "react";
+import styles from '../../styles/imagem/imagem.module.css'
 
 export default function Imagem({ id }) {
   const [image, setImage] = useState('');
@@ -10,8 +11,8 @@ export default function Imagem({ id }) {
   }, []);
 
   return (
-    <div className="component_image">
-      <p>Imagem selecionada: {image.name}</p>
+    <div className={styles.content_image}>
+      <p>Imagem: {image.name}</p>
       <img
         src={`data:${image.type};base64,${image.content}`}
         alt={image.name}
