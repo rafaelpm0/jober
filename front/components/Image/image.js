1d43b9cf3e9e327d0283e0ca9f6e7b2b8d03ed73
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-08-31 18:10:14 -0300

    documantacao compnente image

--- front/components/Image/image.js
+++ front/components/Image/image.js
@@ -3,17 +3,39 @@ import { useEffect, useState } from "react";
 import styles from "../../styles/imagem/imagem.module.css";
 import Loading from "../utilitarios/loading";
 
+/**
+ * Componente para exibir uma imagem associada a um identificador.
+ *
+ * Este componente recupera uma imagem usando a função `getImage` com base no `id` fornecido,
+ * e exibe a imagem quando o carregamento é concluído. Durante o carregamento, exibe um
+ * componente de carregamento (`Loading`).
+ *
+ * @component
+ * @example
+ * <Imagem id={123} />
+ *
+ * @param {Object} props - Propriedades do componente.
+ * @param {number} props.id - ID da imagem a ser exibida.
+ *
+ * @returns {JSX.Element} - Retorna um elemento JSX que representa o conteúdo da imagem.
+ */
 export default function Imagem({ id }) {
-  const [image, setImage] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [image, setImage] = useState(""); // Estado para armazenar a imagem
+  const [loading, setLoading] = useState(true); // Estado para controlar o carregamento
 
-  useEffect(() => {load()}
-  , []);
+  // Efeito colateral para carregar a imagem quando o componente é montado
+  useEffect(() => {
+    load();
+  }, []);
 
+  /**
+   * Função assíncrona para carregar a imagem com base no ID.
+   * Atualiza o estado da imagem e o estado de carregamento.
+   */
   async function load() {
     setLoading(true);
-    const img = await getImage(id);
-    setImage(img)
+    const img = await getImage(id); // Recupera a imagem usando o ID
+    setImage(img);
     setLoading(false);
   }
 
